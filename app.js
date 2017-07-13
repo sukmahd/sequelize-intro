9a29aa2bc7eef6c91ce3a521e6b15d2ba0c3e851
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-07-13 23:47:54 +0700

    update css

--- app.js
+++ app.js
@@ -3,12 +3,15 @@
 const express = require('express');
 const app = express();
 const model = require('./models');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const teachers = require('./routers/teachers');
 const subjects = require('./routers/subjects');
 const students = require('./routers/students');
-const bodyParser = require('body-parser');
 
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
